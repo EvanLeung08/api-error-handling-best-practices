ac1c29671c41cc6c16a2b8070e95d21f4799ef66
Evan43789596 <10856214@163.com>
2019-09-22 21:05:11 +0800

    refactor(#All): remove authentication in controller

--- external-api-error-handling-simple/src/main/java/com/eshare/sample/api/QuotaController.java
+++ external-api-error-handling-simple/src/main/java/com/eshare/sample/api/QuotaController.java
@@ -23,26 +23,20 @@ public class QuotaController {
 
 
   private final QuotaService quotaService;
-  private final AuthenticationService authenticationService;
 
-  public QuotaController(QuotaService quotaService, AuthenticationService authenticationService) {
+  public QuotaController(QuotaService quotaService) {
     this.quotaService = quotaService;
-    this.authenticationService = authenticationService;
   }
 
   @GetMapping(value = "/quota")
   public ResponseEntity<QuotaResponseDTO> getContract(
       @RequestParam(value = "customerId") String customerId) {
-    //Check authentication
-    authenticationService.check();
     //Fetch quota data
     return new ResponseEntity<>(quotaService.getQuota(customerId), HttpStatus.OK);
   }
 
   @PostMapping(value = "/quota")
   public ResponseEntity updateContract(@RequestBody QuotaRequestDTO quotaRequestDTO) {
-    //Check authentication
-    authenticationService.check();
     //Validate request format
     ValidatorUtil.checkBeanParamValidate(quotaRequestDTO);
     return new ResponseEntity(HttpStatus.OK);
